9aad5ed713f32946b714729b26c1c0e3d9ff1281
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2024-07-19 12:46:28 +0530

    Added error detection for NULL

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "headers/constants.h"
 #include "headers/utils.h"
 
 int main(int argc, char *argv[])
 {
-    char* path = get_path(argc, argv);
-    printf("\n%s\n", path);
+    char *path = get_path(argc, argv);
+    if (path == NULL)
+    {
+        perror("PATH: Enter Valid Path");
+        exit(1);
+    }
     return 0;
 }
