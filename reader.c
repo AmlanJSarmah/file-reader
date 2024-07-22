74e7c1e7b996c784a81394602e6571b29215da70
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2024-07-22 17:01:17 +0530

    Added verification method to check if entered path is directory

--- reader.c
+++ reader.c
@@ -9,12 +9,27 @@
 
 void get_data_from_file(char *path, char *buffer)
 {
+    // We check if the path is a directory
+    struct stat stat_buf;
+    if (stat(path, &stat_buf) == -1)
+    {
+        perror("GET: File information");
+        exit(1);
+    }
+    if(S_ISDIR(stat_buf.st_mode))
+    {
+        perror("You Entered a path to directory");
+        exit(1);
+    }
+
     int fd = open(path, O_RDONLY);
     if (!fd)
     {
         perror("OPEN: Cannot open file");
         exit(1);
     }
+
+    // We read the contents
     if (!read(fd, buffer, BUF_SIZE))
     {
         perror("READ: Cannot Read Data");
