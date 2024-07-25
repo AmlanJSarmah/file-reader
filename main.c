5564c77ad5dd83780e3d04411fa6fc290ad987b0
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2024-07-25 08:27:38 +0530

    wrote next item to do

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
     printf("%s\n", buffer);
     while (1)
     {
-        // loop here
+        // TODO: Detech Keypress here to break loop 
     }
     return 0;
 }
