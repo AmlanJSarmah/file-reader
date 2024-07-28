ef212f1e0e4981eb414470fa0a9b20648a5dff08
AmlanJSarmah <amlan.j.sarmah@gmail.com>
2024-07-28 10:58:25 +0530

    The program now function as a normal cat command

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include "headers/constants.h"
 #include "headers/utils.h"
 #include "headers/reader.h"
 
 int main(int argc, char *argv[])
 {
-    // Handle window resize
-    signal(SIGWINCH, window_resize_signal_handle);
     char buffer[BUF_SIZE];
     char *path = get_path(argc, argv);
     if (path == NULL)
@@ -19,9 +16,5 @@ int main(int argc, char *argv[])
     clear();
     get_data_from_file(path, buffer);
     printf("%s\n", buffer);
-    while (1)
-    {
-        // TODO: Detech Keypress here to break loop 
-    }
     return 0;
 }
